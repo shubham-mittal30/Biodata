f47d41be1227e2b8b2a916c5d9732be127dc67cb
shubham mittal <mittalshubham30@gmail.com>
2021-03-06 02:34:28 +0530

    Biodata updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import pic from './profilePic.jpg';
+import pic2 from './profilePic2.jpg';
+import pic3 from './profilePic3.jpg';
+import {
+  faFacebook,
+  faLinkedin,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:"100%",height:"100%"}}>
+
+      <div style={{ display: "inline-block", verticalAlign: "top", width: "21%" }}>
+        <img src={pic} width="100%" alt="profilePic" />
+        <img src={pic2} width="100%" alt="profilePic" />
+        <img src={pic3} width="100%" alt="profilePic" />
+      </div>
+
+      <div className="App" style={{ display: "inline-block", width: "75%" }}>
+
+        <table class="table table-bordered table-hover" >
+          <thead>
+            <tr>
+
+              <th scope="col">NAME</th>
+              <td>Shubham Mittal</td>
+
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">HEIGHT</th>
+              <td>5'6"</td>
+
+            </tr>
+            <tr>
+              <th scope="row">COMPLEXION</th>
+              <td>Fair</td>
+
+            </tr>
+            <tr>
+              <th scope="row">BIRTH DETAILS</th>
+              <td>27 (30<sup>th</sup> Aug 1993) , 12:45 P.M. DEHRADUN ,UTTARAKHAND</td>
+            </tr>
+            <tr>
+              <th scope="row">RELIGION , CASTE & GOTRA</th>
+              <td>Hindu , BISA AGGARWAL & MITTAL</td>
+            </tr>
+            <tr>
+              <th scope="row">EDUCATION</th>
+              <td><b>School:</b> St. Joseph's Academy Dehradun<br /><b>Degree & College:   </b>B.E. Electronics & Communication Engineering (2011-15), Thapar University, Patiala</td>
+            </tr>
+            <tr>
+              <th scope="row">EMPLOYMENT DETAILS</th>
+              <td>Presently Working as <b>Lead Software Engineer</b> @ Airtel Xlabs <b>(Bharti Airtel Limited) Gurgaon</b><br />
+              Prior have worked as Technical Lead @ <b>Newgen Software Technologies Ltd Noida</b> for 3.8 years &<br />
+              As Assistant System Engineer @ <b>Tata Consultancy Services Ltd Gurgaon</b> for 1.8 years</td>
+            </tr>
+            <tr>
+              <th scope="row">ANNUAL INCOME</th>
+              <td>23.0 L.P.A.</td>
+            </tr>
+            <tr>
+              <th scope="row">Hobbies/Interests</th>
+              <td>Reading, Playing Guitar & Table Tennis, Listening Music, Watching Movies & T.V. Series, Travelling & Running</td>
+            </tr>
+            <tr>
+              <th scope="row">FATHER DETAILS</th>
+              <td>Shri Yogesh Kumar Mittal , <b>(M.Com., L.L.B., L.L.M.)</b> <br />
+              Working as <b>Joint Commissioner in SGST Deptt. Govt. of Uttarakhand, Kashipur</b></td>
+            </tr>
+            <tr>
+              <th scope="row">MOTHER DETAILS</th>
+              <td>Smt. Anjali Mittal , <b>(M.A. Economics)</b> <br />
+              HouseMaker (Religious & Pious Lady with high moral values)</td>
+            </tr>
+            <tr>
+              <th scope="row">SISTER DETAILS</th>
+              <td>Mrs. Aanchal Agarwal , <b>(M.B.A in Information Systems Management, U.P.E.S, Dehradun)</b> <br />
+              Working as Assistant Manager Finance in Conduent Services Noida <br />Married to Mr. Shashank Agarwal <b>(Director of Gray Graph Technologies Noida.)</b></td>
+            </tr>
+            <tr>
+              <th scope="row">GRAND PARENT DETAILS</th>
+              <td>Late Shri Satya Prakash Mittal & Late Smt. Jai Mala Devi</td>
+            </tr>
+            <tr>
+              <th scope="row">TAUJI DETAILS</th>
+              <td>Dr. Umesh Kumar Mittal -  Residing in Saharanpur, U.P.<br />
+              Shri Rakesh Kumar Mittal - Retired from Senior DGM from EIL and currently residing in Mumbai<br />
+              Dr. Rajesh Kumar Mittal - Retired from DGM in ONGC and currently residing in Gurgaon</td>
+            </tr>
+            <tr>
+              <th scope="row">MAMAJI DETAILS</th>
+              <td>Shri Vinod Kumar Singhal - Engaged in own business of bricks MFG. & Jewellery in Dehradun<br />
+              Shri Pramod Singhal - Engaged in own business of bricks MFG. & Polymer trading in Dehradun (Shivalik Bricks and Gauri Shankar Polymers)<br />
+              Shri Atul Singhal - Engaged in own business of C&F & Distribution of Pharma MNC’s & Amaron Batteries for Uttarakhand. Also deal in Real Estates Building and Consultation in Dehradun.</td>
+            </tr>
+            <tr>
+              <th scope="row">MAUSAJI DETAILS</th>
+              <td>Shri Sanjeev Goyal - Engaged in own business of MFG. & export of textiles in Panipat. (Peerless International)</td>
+            </tr>
+            <tr>
+              <th scope="row">BUAJI DETAILS</th>
+              <td>All five Buas' are well settled and placed in their respective families.</td>
+            </tr>
+            <tr>
+              <th scope="row">OTHER DETAILS</th>
+              <td>We have our own house, car in Dehradun and
+currently residing in Kashipur. Should the above particulars find favour with you, kindly send similar details about the
+girl & the family at the following address – <a href = "mailto: mittalshubham30@gmail.com">mittalshubham30@gmail.com</a></td>
+            </tr>
+            <tr>
+              <th scope="row">SOCIAL FOLLOW </th>
+              <td>
+                <div class="social-container">
+
+                  <a href="https://www.facebook.com/shubham.mittal.988/"
+                    className="facebook social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                  </a>
+                  <a href="https://www.instagram.com/shubh_on_strings/"
+                    className="instagram social">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/shubham-mittal-97883b34/"
+                    className="linkedIn social">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                  </a></div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
